09bb70516a5204cf1f0950cc73fdb85a7b59b0ca
“aks.bsyl@gmai.com <aks.bsyl@gmail.com>
2023-03-28 18:41:52 -0400

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
+import { HomeComponent } from './home/home.component';
   
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path: 'login', component:LoginComponent},
+  {path: 'signup', component:SignupComponent},
+  {path:'dashboard', component:EmployeeDashboardComponent},
+  {path:'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,34 @@ import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashbo
 // import { SignupComponent } from './signup/signup.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { DropDownList, DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeDashboardComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+
     // LoginComponent,
     // SignupComponent
   ],
   imports: [
     BrowserModule,
+    DropDownListModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
